42ad6fbc2a93ade51202c2d5751626b87e03e1c6
Ronak Chaudhari <81430378+RonakChaudhari@users.noreply.github.com>
2023-06-06 23:16:16 +0530

    Add files via upload

---
+++ Module1/3-6/DisplayInfo.java
@@ -0,0 +1,12 @@
+public class DisplayInfo{
+
+    public static void main(String[] args) {
+
+        System.out.println("Name : Ronak Chaudhari");
+        System.out.println("Birth Date : 24/05/2003");
+        System.out.println("Age : 20");
+        System.out.println("Address : Antilia, Scod Tower,\n Morbi Road, Rajkot - 360003.");
+    }
+
+}
+
