357a32ec0fa42e10b5866547de069cee9666bfe6
Ronak Chaudhari <81430378+RonakChaudhari@users.noreply.github.com>
2023-06-08 09:48:20 +0530

    Update ReverseOrderOfNumber.java

--- Module1/6-6/ReverseOrderOfNumber.java
+++ Module1/6-6/ReverseOrderOfNumber.java
@@ -10,26 +10,14 @@ public class ReverseOrderOfNumber {
         System.out.print("Enter Number : ");
         num = sc.nextInt(); 
 
-        // 0 thi mota 
         while(num>0)
         {
-            // num ne 10 vade divide kari modulo ne a ma store kari 
             m = num % 10; 
-
-            // (0*10) + modulo; 
             rev = (rev*10) + m; 
-
             num = num/10; 
         }
-
         System.out.println("Reverse value is : " + rev);
 
     }    
 }
 
-/* 
- 
-Enter Number : 123
-Reverse value is : 321
-
- */
