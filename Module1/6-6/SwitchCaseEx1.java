b6aaecbf9c7be77e6118c8e46173a32bf4ec88f7
Ronak Chaudhari <81430378+RonakChaudhari@users.noreply.github.com>
2023-06-08 11:32:24 +0530

    Update SwitchCaseEx1.java

--- Module1/6-6/SwitchCaseEx1.java
+++ Module1/6-6/SwitchCaseEx1.java
@@ -13,25 +13,25 @@ public class SwitchCaseEx1 {
         switch(day)
         {
             case 1: 
-                System.out.println(" Monday");
+                System.out.println("Monday");
                 break; 
             case 2: 
-                System.out.println(" Tuesday");
+                System.out.println("Tuesday");
                 break; 
             case 3: 
-                System.out.println(" Wednesday");
+                System.out.println("Wednesday");
                 break; 
             case 4: 
-                System.out.println(" Thursday");
+                System.out.println("Thursday");
                 break; 
             case 5: 
-                System.out.println(" Friday");
+                System.out.println("Friday");
                 break; 
             case 6: 
-                System.out.println(" Saturday");
+                System.out.println("Saturday");
                 break; 
             case 7: 
-                System.out.println(" Sunday");
+                System.out.println("Sunday");
                 break; 
             default: 
                 System.out.println("INVALID VALUE...");
