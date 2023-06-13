05a197d3f01851257962a4210af63c2698043fbe
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-06-13 11:16:48 +0530

    Update CountNumberOfDigits.java

--- Module2/CountNumberOfDigits.java
+++ Module2/CountNumberOfDigits.java
@@ -1,10 +1,18 @@
 
+import java.util.Scanner;
+
 public class CountNumberOfDigits {
+
 	public static void main(String[] args) {
 		
-		int num = 0003452, count = 0; 
+		int num, count = 0; 
 		
-		while(num != 0) {
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println(" Input an integer number\n less than ten billion: 125463");
+		num = sc.nextInt();
+		
+		while(num != 0){
 			num /= 10; 
 			++count; 
 		}
