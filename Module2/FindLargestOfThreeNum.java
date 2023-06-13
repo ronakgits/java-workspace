22300593b3c86f4924f355415ad69ca98abbe643
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-06-13 10:15:18 +0530

    Create FindLargestOfThreeNum.java

---
+++ Module2/FindLargestOfThreeNum.java
@@ -0,0 +1,47 @@
+package module2;
+
+import java.util.Scanner;
+
+public class FindLargestOfThreeNum {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		int n1, n2, n3; 
+		
+		System.out.print("Enter 1 Number : ");
+		n1 = sc.nextInt(); 
+		
+		System.out.print("Enter 2 Number : ");
+		n2 = sc.nextInt(); 
+		
+		System.out.print("Enter 3 Number : ");
+		n3 = sc.nextInt(); 
+		
+		if(n1 > n2 && n1 > n3) 
+		{
+			System.out.println(n1 + " is the largest number.");
+		}
+		else if(n2 > n1 && n2 > n3) 
+		{
+			System.out.println(n2 + " is the largest number.");
+		}
+		else if(n3 > n1 && n3 > n2) 
+		{
+			System.out.println(n3 + " is the largest number.");
+		}
+		else if(n1 == n2 && n2 == n3) 
+		{
+			System.out.println("Numbers are same.");
+		}
+		else if(n1 == n2)
+		{
+			System.out.println(" 1 & 2 Numbers are same.");
+		}
+		else if(n2 == n3) 
+		{
+			System.out.println(" 2 & 3 Numbers are same.");
+		}
+	}
+}
