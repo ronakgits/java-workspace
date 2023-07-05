d317637bdb91e75563668ed41ce878645d374aa3
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-07-05 20:53:34 +0530

    Add files via upload

---
+++ Assignment/java_introduction/Ex9.java
@@ -0,0 +1,17 @@
+public class Ex9 
+{
+    public static void main(String[] y) 
+    {
+        
+        int year = 2000; 
+
+        if(year % 4 == 0)
+        {
+            System.out.println("Leap Year");
+        }
+        else
+        {
+            System.out.println("Not Leap Year");
+        }
+    }
+}
