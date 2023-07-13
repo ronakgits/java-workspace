e30bfb2b595795c6e70c9181c4769c1fa0b7aca9
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-07-13 09:48:12 +0530

    Delete Module2 directory

--- Module2/EndsWithEx1.java
+++
@@ -1,9 +0,0 @@
-public class EndsWithEx1
-{  
-    public static void main(String args[])
-    {  
-        String s1 = "Java Exercises";  
-    
-        System.out.println(s1.endsWith("se"));  
-    }
-}  
