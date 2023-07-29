8a4f9486404cd3edf8128d29437d45230bd1c6aa
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-07-29 14:10:59 +0530

    Delete Assignment/Core Java directory

--- Assignment/Core Java/pg2/Ex12.java
+++
@@ -1,9 +0,0 @@
-
-public class Ex12 {
-	
-    public static void main(String[] args) {
-
-        System.out.println(java.time.LocalTime.now());    
-    }      
-
-}  
--- Assignment/Core Java/pg4/Ex33.java/Shape.java
+++
@@ -1,8 +0,0 @@
-abstract class Shape {
-    
-    public abstract double RectangleArea(double length, double breadth);
-
-    public abstract double SquareArea(double side);
-
-    public abstract double CircleArea(double radius);
-}
