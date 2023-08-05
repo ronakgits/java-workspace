4d13b80870d16da8ea0dd9b73bffe09cf548b176
Ronak Chaudhari <81430378+ronakgits@users.noreply.github.com>
2023-08-05 10:30:18 +0530

    Update Ex20.java

--- Assignment/Core Java/pg2/Ex20.java
+++ Assignment/Core Java/pg2/Ex20.java
@@ -1,12 +1,44 @@
-
 public class Ex20 {
-    
-    public static void main(String[] args) {
-        
-        String s = "successes"; 
 
-        char c = s.charAt(2);
+	static final int NOOFCHARS = 256;	// ASCII value 
+	
+	static char get2ndMostFreq(String str1) {
 
-        System.out.println("The second most frequent char in the string is: " + c);
-    }
+		int[] ctr = new int[NOOFCHARS];
+		
+		int i;
+
+		for (i = 0; i < str1.length(); i++)
+			(ctr[str1.charAt(i)]) ++;
+
+		int ctr_first = 0, ctr_second = 0;
+
+		for (i = 0; i < NOOFCHARS; i++) {
+
+			if (ctr[i] > ctr[ctr_first]) {
+
+				ctr_second = ctr_first;
+				ctr_first = i;
+			
+			} else if (ctr[i] > ctr[ctr_second] && ctr[i] != ctr[ctr_first])
+
+				ctr_second = i;
+		}
+
+		return (char) ctr_second;
+	}
+	 
+	public static void main(String args[]) {
+	
+	String str1 = "successes";
+		
+	System.out.println("The given string is: " + str1);
+	
+	char res = get2ndMostFreq(str1);
+	
+	if (res != '\0')
+			System.out.println("The second most frequent char in the string is: " + res);
+	else
+			System.out.println("No second most frequent character found in the string.");
+	}
 }
